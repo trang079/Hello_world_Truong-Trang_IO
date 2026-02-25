8ca26e56071f76ab00b082e15008dce01730b59f
Чыонг Тхи Тхюи Чанг <chyong.t@edu.spbstu.ru>
2026-02-26 02:57:08 +0300

    Delete task_2-0_5.py.py

--- task_2-0_5.py.py
+++
@@ -1,8 +0,0 @@
-name = "Чанг"
-group = "4731204/50001"
-score = 5
-print(f"Студент {name} из группы {group} получил {score} баллов")
-
-f = open("result.txt", "w", encoding="utf-8")
-print("Результаты работы", file=f)
-f.close()
