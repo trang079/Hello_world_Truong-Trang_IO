3a14a00d0367e8c7066206bb1655f6e8626bc144
Чыонг Тхи Тхюи Чанг <chyong.t@edu.spbstu.ru>
2026-02-26 00:43:25 +0300

    projects_2

---
+++ task_2-0_4.py.py
@@ -0,0 +1,9 @@
+print("Предмет\tОценка")
+print("Математика\t5")
+print("Физика\t4")
+print("Информатика\t5")
+
+name = "Чанг"
+surname = "Чыонг Тхи Тхюи"
+age = 35
+print(f"\nМеня зовут {surname} {name}, мне {age} лет")
