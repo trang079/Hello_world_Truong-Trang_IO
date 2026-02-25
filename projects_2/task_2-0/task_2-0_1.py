590037f4cc6c074adbdbb2edf1c1b0e4d0cfd07c
Чыонг Тхи Тхюи Чанг <chyong.t@edu.spbstu.ru>
2026-02-26 01:55:47 +0300

    Update and rename task_2-0_1.py.py to projects_2/task_2-0/task_2-0_1.py

---
+++ projects_2/task_2-0/task_2-0_1.py
@@ -0,0 +1,2 @@
+print('Hello World!')
+print('Фамилия:', 'Truong Thi Thuy', 'Имя:', 'Trang', 'Возраст:', 35)
--- task_2-0_1.py.py
+++
@@ -1,2 +0,0 @@
-print('Hello World!')
-print('Фамилия:', 'Truong Thi Thuy', 'Имя:', 'Trang', 'Возраст:', 35)
