856f0606113cd2ba11a8ddeaf60b89162e9d7016
Чыонг Тхи Тхюи Чанг <chyong.t@edu.spbstu.ru>
2026-06-09 16:46:21 +0300

    Update replace.sh

--- projects_6_6-9-2026/task_6_6-9-2026/replace.sh
+++ projects_6_6-9-2026/task_6_6-9-2026/replace.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 FILE_PATH="config.txt"
+
 if [ -f "$FILE_PATH" ]; then
-    sed -i 's/\bname\b/user_name/g' "$FILE_PATH"
+    # Добавлен флаг -E, чтобы команда sed правильно распознавала границы слова \b
+    sed -i -E 's/\bname\b/user_name/g' "$FILE_PATH"
     echo "Замена успешно выполнена"
 else
     echo "Ошибка: Файл не найден."
 fi
+
